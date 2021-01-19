cc3c846540f0bced75d76fb35b302af57ff98f93
chaenabi <find1086@gmail.com>
2021-01-19 18:13:19 +0900

    filtered contents

--- index.js
+++ index.js
@@ -5,33 +5,45 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const dcinside = 'https://gall.dcinside.com'
-const input = '!';
+const input = '여소환';
 const keyword = encodeURI(input);
 const recommend = '&exception_mode=recommend';
-let path = '/board/lists/?id=bns&s_type=search_subject_memo&s_keyword=' + keyword + recommend;
+let path = '/board/lists/?id=bns&s_type=search_name&s_keyword=' + keyword + recommend;
 let url = dcinside + path;
-let result = "";
+let title = "";
+let andromeda_cnt = 0;
+let date = "";
 
 client.fetch(url, {}, function (error, $, res, body) {
-  //  console.log('error:', error);
-  //  console.log('$:', $);
+  //  console.log('error:', error)
+  //  console.log('$:', $)
 });
 
 const getPostContent = (url) => {
     request({ url: url }, function (error, response, body) {
-       const dom = new JSDOM(body);
-        console.log(path)
-       if ((search_next_btn = dom.window.document.querySelector('.search_next')) !== null) {
-          const tbody = dom.window.document.querySelectorAll('tr.ub-content, tr.ub-content.gall_tit ub-word');
+       const dom = new JSDOM(body)
+     
+        if ((search_next_btn = dom.window.document.querySelector('.search_next')) !== null) {
+          const tbody = dom.window.document.querySelectorAll('tr.ub-content')
           tbody.forEach((e) => {
-                    result += e.innerHTML
+                     if (dom.window.document.querySelector('.ub-writer').getAttribute('user_name') !== '운영자') {  
+                        date = e.querySelectorAll('td')[3].getAttribute('title')
+                       title = new JSDOM(e.innerHTML).window.document.querySelector('a').innerHTML.split('</em>')[1]
+                       andromeda_cnt++
+                       console.log(`[게시물 제목] ${title}`)
+                       console.log(`[날       짜] ${date}`)
+                       console.log(`[갯수 카운트] ${andromeda_cnt}`)
+                       console.log('---------------------------')
+                    }
+                    else dom.window.document.querySelector('.ub-writer').setAttribute('user_name', '');
+                    
           })
-         path = search_next_btn.href;
-         url = dcinside + path;
-         getPostContent(url); // 비동기식이라. path가 변하지 않아 무한루프
-        console.log(result);
+         //console.log(path)
+         path = search_next_btn.href
+         url = dcinside + path
+         getPostContent(url)
        }
-    });    
+    }); 
 };
 
-getPostContent(url);
+getPostContent(url)
